d283dc76822d30b426d49e31e8c75dff5a2cfff1
Jack Humbert <jack.humb@gmail.com>
2023-05-05 11:18:24 -0400

    no ref on path

--- src/Main.cpp
+++ src/Main.cpp
@@ -49,7 +49,7 @@ pugi::xml_document inputUserMappingsOriginal;
 
 static std::vector<std::filesystem::path> document_paths;
 
-void Add(std::filesystem::path &path) {
+void Add(std::filesystem::path path) {
     spdlog::info(L"Will load document: {}", path.c_str()); 
     document_paths.emplace_back(path); 
 }
