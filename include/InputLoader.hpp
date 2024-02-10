826e08edb4eb47fd1096dc9e91fdb9703ddde689
Maxime <672982+maximegmd@users.noreply.github.com>
2024-02-10 20:04:39 +0100

    Update InputLoader.hpp

--- include/InputLoader.hpp
+++ include/InputLoader.hpp
@@ -12,6 +12,6 @@
 
 namespace InputLoader {
 
-DLLDIR void Add(RED4ext::PluginHandle aHandle, const wchar_t * str);
+DLLDIR extern "C" void Add(RED4ext::PluginHandle aHandle, const wchar_t * str);
 
 }
